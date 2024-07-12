422078e594be4c50814a04df58a6159f3212a612
Aaditya Raj Pandey <122453206+aaditya620321@users.noreply.github.com>
2024-07-12 11:02:55 +0530

    Zamoto Analysis.py

--- Zamoto Analysis.py
+++ Zamoto Analysis.py
@@ -5,13 +5,13 @@ import pandas as pd
 import numpy as np
 
 # Loading .csv file of zomato data using pandas
-df_data = pd.read_csv(r"C:\Users\ritik\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\Zamoto raw data\zomato.csv", encoding='unicode_escape')
+df_data = pd.read_csv(r"C:\Users\aaditya\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\Zamoto raw data\zomato.csv", encoding='unicode_escape')
 print(df_data)
 
 
 # Loading excel file of country code using pandas
 
-df_country = pd.read_excel(r"C:\Users\ritik\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\Zamoto raw data\Country-Code.xlsx")
+df_country = pd.read_excel(r"C:\Users\aaditya\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\Zamoto raw data\Country-Code.xlsx")
 print(df_country)
 
 
@@ -77,5 +77,5 @@ print(df.Country.value_counts())
 
 
 #downlaod data
-#df.to_csv(r"C:\Users\ritik\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\zomato.csv")
+#df.to_csv(r"C:\Users\aaditya\OneDrive\Documents\project data analysis\Power bi project\Zamoto project\zomato.csv")
 print(df.to_string)
